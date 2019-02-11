c04e19c0b69fdcad836b27fb4baa8cc1f3a7bbdf
Faiz Abbas <faiz_abbas@hotmail.co.uk>
2019-02-11 13:26:55 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+def dictionary
+  dictionary = {
+    "too" => "2",
+    "to" => "2",
+    "two" =>"2",
+    "four" => "4",
+    "for" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+
+def word_substituter(tweet)
+  tweet.split.map do |word|
+    
